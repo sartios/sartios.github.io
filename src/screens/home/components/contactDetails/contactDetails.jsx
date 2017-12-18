5e5af0a9a231fa5988523488be8fb8d375f4711a
sartios <sartios@hotmail.com>
2017-12-18 11:47:26 +0200

    create list & list item components

--- src/screens/home/components/contactDetails/contactDetails.jsx
+++ src/screens/home/components/contactDetails/contactDetails.jsx
@@ -4,36 +4,38 @@ import Icon from 'components/atoms/icon';
 import Image from 'components/atoms/image';
 import TextIcon from 'components/molecules/textIcon';
 import IconLink from 'components/molecules/iconLink';
+import List from 'components/molecules/list';
 import ContactButton from './contactButton/contactButton';
 
 import './contactDetails.post.css';
 
-const ContactDetails = () => (
-  <div className="sa-portfolio-contact-details-container">
-    <div className="sa-portfolio-general-details">
-      <Image circular />
-      <div>Savramis Sartios</div>
-      <div>Software Developer</div>
+const ContactDetails = () => {
+  const email = (<TextIcon icon="envelope" text="savramis.sartios@gmail.com" />);
+  const linkedin = (<TextIcon icon="linkedin" text="linkedin.com/sartios" />);
+
+  const items = [{ component: email }, { component: linkedin }];
+
+
+  return (
+    <div className="sa-portfolio-contact-details-container">
+      <div className="sa-portfolio-general-details">
+        <Image circular />
+        <div>Savramis Sartios</div>
+        <div>Software Developer</div>
+      </div>
+      <div className="sa-portfolio-buttons-container">
+        <ContactButton />
+      </div>
+      <div className="sa-portfolio-main-social">
+        <IconLink
+          icon="github"
+          link="http://www.github.com/sartios"
+          text="github.com/sartios"
+        />
+      </div>
+      <List items={items} />
     </div>
-    <div className="sa-portfolio-buttons-container">
-      <ContactButton />
-    </div>
-    <div className="sa-portfolio-main-social">
-      <IconLink
-        icon="github"
-        link="http://www.github.com/sartios"
-        text="github.com/sartios"
-      />
-    </div>
-    <ul className="sa-portfolio-secondary-social">
-      <li>
-        <TextIcon icon="envelope" text="savramis.sartios@gmail.com" />
-      </li>
-      <li>
-        <TextIcon icon="linkedin" text="linkedin.com/sartios" />
-      </li>
-    </ul>
-  </div>
-);
+  );
+};
 
 export default ContactDetails;
