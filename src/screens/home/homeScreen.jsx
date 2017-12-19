b2540ff5bd1701abb79db49f566153e3ebfcaa0e
sartios <sartios@hotmail.com>
2017-12-19 11:15:50 +0200

    update home grid widths

--- src/screens/home/homeScreen.jsx
+++ src/screens/home/homeScreen.jsx
@@ -11,19 +11,19 @@ import './homeScreen.post.css';
 const HomeScreen = () => (
   <Grid celled>
     <Grid.Row>
-      <Grid.Column width={13}>
+      <Grid.Column computer={13} mobile={16} tablet={16}>
         <Navbar />
       </Grid.Column>
     </Grid.Row>
 
     <Grid.Row className="sp-home-main-row">
-      <Grid.Column width={3}>
+      <Grid.Column computer={3} mobile={16} tablet={16}>
         <ContactDetails />
       </Grid.Column>
-      <Grid.Column width={10}>
+      <Grid.Column computer={10} mobile={16} tablet={16}>
     Overview
       </Grid.Column>
-      <Grid.Column width={3}>
+      <Grid.Column computer={3} mobile={16} tablet={16}>
     Skills
       </Grid.Column>
     </Grid.Row>
