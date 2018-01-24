0dfcbf61c8a3df486d82958e5ec24d3196676d93
sartios <sartios@hotmail.com>
2018-01-24 12:07:39 +0200

    remove overview post css

--- src/screens/home/components/overview/overview.jsx
+++ src/screens/home/components/overview/overview.jsx
@@ -5,8 +5,6 @@ import DemoApps from './demoApps/demoApps';
 import Education from './education/education';
 import WorkExperience from './workExperience/workExperience';
 
-import './overview.post.css';
-
 const Overview = () => (
   <Grid>
     <Grid.Row>
