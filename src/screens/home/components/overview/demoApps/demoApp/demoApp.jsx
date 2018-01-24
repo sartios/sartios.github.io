e564594d31f0e58a0bebe3ae23f06429753706f2
sartios <sartios@hotmail.com>
2018-01-24 12:48:07 +0200

    migrate demo app to styled components

--- src/screens/home/components/overview/demoApps/demoApp/demoApp.jsx
+++ src/screens/home/components/overview/demoApps/demoApp/demoApp.jsx
@@ -1,17 +1,46 @@
 import React from 'react';
+import styled from 'styled-components';
 
-import './demoApp.post.css';
+const DemoApp = () => {
+  const Link = styled.a`
+    display: inline-block;
+    width: 100px;
+    height: 60px;
+    line-height: 60px;
+    border: 1px solid lightgrey;
+    text-decoration: none;
+    color: #3F4548;
+    text-align: center;
+    vertical-align: center;
+  `;
 
-const DemoApp = () => (
-  <li className="sa-portfolio-demo-app">
-    <a className="sa-portfolio-demo-app-link" href="#">
-      App Logo
-    </a>
-    <div className="sa-portfolio-demo-app-metadata">
-      <span className="sa-portfolio-demo-app-name">App Name</span>
-      <span className="sa-portfolio-demo-app-tech">React</span>
-    </div>
-  </li>
-);
+  const Metadata = styled.div`
+    margin-top: 10px;
+  `;
+
+  const Name = styled.span`
+    display: block;
+    font-weight: bold;
+    cursor: default;
+  `;
+
+  const Tech = styled.span`
+    display: block;
+    color: #9A9B9D;
+    cursor: default;
+  `;
+
+  return (
+    <li>
+      <Link href="#">
+        App Logo
+      </Link>
+      <Metadata>
+        <Name>App Name</Name>
+        <Tech>React</Tech>
+      </Metadata>
+    </li>
+  );
+};
 
 export default DemoApp;
