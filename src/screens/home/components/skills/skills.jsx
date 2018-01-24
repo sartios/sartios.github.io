331c3a50adf2a00f5f959e7af64ac23e48cedd76
sartios <sartios@hotmail.com>
2018-01-24 12:17:59 +0200

    migrate skills to styled components

--- src/screens/home/components/skills/skills.jsx
+++ src/screens/home/components/skills/skills.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { Input } from 'semantic-ui-react';
 
 import Icon from 'components/atoms/icon/icon';
-
-import './skills.post.css';
 
 class Skills extends Component {
   constructor(props) {
@@ -14,69 +13,118 @@ class Skills extends Component {
   }
 
   render() {
+    const Container = styled.div`
+      background-color:#3F4548;
+      color: lightgrey;
+      position: absolute;
+      top: 0;
+      right: 0;
+      width: 20%;
+      z-index: 10;
+      height: 1024px;
+
+      @media (max-width: 992px) {
+        position: relative;
+        width: 100%;
+        height: 500px;
+      }
+    `;
+
+    const ListItem = styled.div`
+      padding-left: 20px;
+      padding-top: 10px;
+      padding-right: 20px;
+    `;
+
+    const SearchBarContainer = styled.div`
+      input {
+        color: lightgray !important;
+      }
+    
+      i {
+        color: lightgrey !important;
+      }
+    `;
+
+    const ListItemContent = styled.div`
+      height: 150px;
+    `;
+
+    const ListItemTitle = styled.div`
+      border-bottom: 1px solid lightgrey;
+      margin-bottom: 10px;
+      padding-bottom: 10px;
+      text-transform: uppercase;
+    `;
+
+    const ListItemSkill = styled.div`
+      display: flex;
+      justify-content: space-between;
+    `;
+
     return (
-      <div className="sa-portfolio-skills-container">
-        <div className="sa-portfolio-skills-list-item">
-          <div className="sa-portfolio-skills-search-bar">
+      <Container>
+        <ListItem>
+          <SearchBarContainer>
             <Input icon="search" placeholder="Search..." transparent inverted />
-          </div>
-        </div>
-        <div className="sa-portfolio-skills-list-item">
-          <div className="sa-portfolio-skills-list-item-content">
-            <div className="sa-portfolio-skills-list-item-title">front end</div>
-            <div className="sa-portfolio-skill">
+          </SearchBarContainer>
+        </ListItem>
+        <ListItem>
+          <ListItemContent>
+            <ListItemTitle>front end</ListItemTitle>
+            <ListItemSkill>
               HTML, CSS, JS
               <Icon name="check" color="green-light" />
-            </div>
-            <div className="sa-portfolio-skill">
-            React
+            </ListItemSkill>
+            <ListItemSkill>
+              React
               <Icon name="check" color="green-light" />
-            </div>
-            <div className="sa-portfolio-skill">
-            Redux
+            </ListItemSkill>
+            <ListItemSkill>
+              Redux
               <Icon name="check" color="green-light" />
-            </div>
-            <div className="sa-portfolio-skill">
-            GraphQL
+            </ListItemSkill>
+            <ListItemSkill>
+              GraphQL
               <Icon name="check" color="green-light" />
-            </div>
-          </div>
-        </div>
-        <div className="sa-portfolio-skills-list-item">
-          <div className="sa-portfolio-skills-list-item-content">
-            <div className="sa-portfolio-skills-list-item-title">back end</div>
-            <div className="sa-portfolio-skill">
-            Java, JavaScript, C#
+            </ListItemSkill>
+          </ListItemContent>
+        </ListItem>
+        <ListItem>
+          <ListItemContent>
+            <ListItemTitle>back end</ListItemTitle>
+            <ListItemSkill>
+              Java, JavaScript, C#
               <Icon name="check" color="green-light" />
-            </div>
-            <div className="sa-portfolio-skill">
-            REST
+            </ListItemSkill>
+            <ListItemSkill>
+              REST
               <Icon name="check" color="green-light" />
-            </div>
-            <div className="sa-portfolio-skill">
-            Serverless
+            </ListItemSkill>
+            <ListItemSkill>
+              Serverless
               <Icon name="check" color="green-light" />
-            </div>
-          </div>
-        </div>
-        <div className="sa-portfolio-skills-list-item">
-          <div className="sa-portfolio-skills-list-item-content">
-            <div className="sa-portfolio-skills-list-item-title">miscellaneous</div>
-            <div className="sa-portfolio-skill">
-            Travelling
+            </ListItemSkill>
+          </ListItemContent>
+        </ListItem>
+        <ListItem>
+          <ListItemContent>
+            <ListItemTitle>miscellaneous</ListItemTitle>
+            <ListItemSkill>
+              Travelling
               <Icon name="star" color="orange" />
-            </div>
-            <div className="sa-portfolio-skill">
-            Fitness
+            </ListItemSkill>
+            <ListItemSkill>
+              Fitness
               <Icon name="star" color="orange" />
-            </div>
-            <div className="sa-portfolio-skill">
-            Contributing
+            </ListItemSkill>
+            <ListItemSkill>
+              Contributing
               <Icon name="star" color="orange" />
-            </div>
-          </div>
-        </div>
-      </div>
+            </ListItemSkill>
+          </ListItemContent>
+        </ListItem>
+      </Container>
     );
   }
 }
